9d8b8e3723b4a349f5877cbeebb204b121eff72d
AdamMonroUnil <adam.monro@unil.ch>
2023-12-04 11:34:49 +0100

    Update ZOOMApp.py

--- Code/Streamlit/ZOOMApp.py
+++ Code/Streamlit/ZOOMApp.py
@@ -7,13 +7,13 @@ import joblib
 @st.cache(allow_output_mutation=True)
 def load_model():
     model = CamembertForSequenceClassification.from_pretrained('camembert-base', num_labels=6)
-    model.load_state_dict(torch.load('C:\Users\Adam\OneDrive\Desktop\Uni\UNIL\Semestre 1\Data Science & Machine Learning\Assignments\camembert_model.pth', map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(r'C:\Users\Adam\OneDrive\Desktop\Uni\UNIL\Semestre 1\Data Science & Machine Learning\Assignments\camembert_model.pth', map_location=torch.device('cpu')))
     model.eval()
     return model
 
 @st.cache(allow_output_mutation=True)
 def load_label_encoder():
-    return joblib.load('C:\Users\Adam\OneDrive\Desktop\Uni\UNIL\Semestre 1\Data Science & Machine Learning\Assignments\label_encoder.joblib')
+    return joblib.load(r'C:\Users\Adam\OneDrive\Desktop\Uni\UNIL\Semestre 1\Data Science & Machine Learning\Assignments\label_encoder.joblib')
 
 model = load_model()
 label_encoder = load_label_encoder()
@@ -29,6 +29,11 @@ def predict_difficulty(text):
 
 # Streamlit interface
 st.title('Welcome to LingoRank!')
+
+# Displaying the Zoom Logo
+zoom_logo_url = "https://1000logos.net/wp-content/uploads/2021/06/Zoom-Logo.png"
+st.image(zoom_logo_url, width=100)
+
 st.write("""
 ### Overview
 LingoRank is a revolutionary startup aimed at enhancing foreign language learning. Our tool helps English speakers to improve their French by reading texts that match their language proficiency level. 
