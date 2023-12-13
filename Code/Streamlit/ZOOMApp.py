2a725091e9b800e6fd6b83a57b2f7e85813947ab
cailenhughes <cailenhughes@icloud.com>
2023-12-13 13:20:49 +0100

    App streamlit + model

--- Code/Streamlit/ZOOMApp.py
+++ Code/Streamlit/ZOOMApp.py
@@ -4,10 +4,8 @@ from transformers import CamembertTokenizer, CamembertForSequenceClassification
 import joblib
 import gdown
 import os
-from googleapiclient.discovery import build
 from langdetect import detect
-import requests
-from xml.etree import ElementTree
+from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound
 
 # Function to download a file from Google Drive
 def download_file_from_google_drive(url, output_path):
@@ -50,6 +48,24 @@ def predict_difficulty(text):
     difficulty = label_encoder.inverse_transform([predictions.item()])[0]
     return difficulty
 
+def analyze_subtitles(subtitles):
+    if detect(subtitles) == 'fr':
+        return predict_difficulty(subtitles)
+    else:
+        return "Subtitles are not in French."
+
+def fetch_and_analyze_subtitles(video_url):
+    video_id = video_url.split('v=')[-1]  # Extract video ID from URL
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        transcript = transcript_list.find_transcript(['fr']).fetch()
+        subtitles = ' '.join([entry['text'] for entry in transcript])
+        return analyze_subtitles(subtitles)
+    except NoTranscriptFound:
+        return "No subtitles found for this video."
+    except Exception as e:
+        return str(e)
+
 # Set the global background color to white
 def set_global_background_color(background_color):
     st.markdown(
@@ -63,97 +79,54 @@ def set_global_background_color(background_color):
         unsafe_allow_html=True
     )
 
-# Setup YouTube API
-youtube_api_key = 'API'  
-youtube = build('youtube', 'v3', developerKey=youtube_api_key)
+set_global_background_color("white")  # Set global background to white
 
-def get_video_id(video_url):
-    return video_url.split('v=')[-1]
+# Creating columns for layout
+left_column, right_column = st.columns([1, 2])
 
-def get_captions_from_url(video_id):
-    request = youtube.captions().list(
-        part="snippet",
-        videoId=video_id
+# Using the left column for logos with a light blue background
+with left_column:
+    st.markdown(
+        f"""
+        <div style="background-color:#F0F8FF;padding:10px;border-radius:5px;">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2b/Logo_Universit%C3%A9_de_Lausanne.svg" 
+                 alt="University of Lausanne logo" style="width:100%">
+            <img src="https://1000logos.net/wp-content/uploads/2021/06/Zoom-Logo.png" 
+                 alt="Zoom logo" style="width:100%">
+        </div>
+        <div style="background-color:#F0F8FF;height:625px;"></div>
+        """,
+        unsafe_allow_html=True
     )
-    response = request.execute()
 
-    for item in response.get("items", []):
-        if item["snippet"]["language"] == "fr":
-            caption_id = item["id"]
-            break
-    else:
-        return None
+# Using the right column for the main app interface
+with right_column:
+    st.title('Welcome to LingoRank!')
 
-    caption_request = youtube.captions().download(
-        id=caption_id,
-        tfmt='ttml'
-    )
-    caption_response = caption_request.execute()
+    st.write("""
+    ### Overview
+    LingoRank is a revolutionary startup aimed at enhancing foreign language learning. Our tool helps English speakers to improve their French by reading texts that match their language proficiency level. 
 
-    # Parsing and extracting text from TTML (XML-based format)
-    captions = ''
-    root = ElementTree.fromstring(caption_response)
-    for elem in root.iter('{http://www.w3.org/ns/ttml}body'):
-        for p in elem:
-            captions += p.text + ' '
+    Finding texts with the appropriate difficulty level (A1 to C2) can be challenging. LingoRank solves this by predicting the difficulty of French texts, aiding learners in choosing materials that are neither too easy nor too hard. 
 
-    return captions
+    Simply enter a French text below, and LingoRank will evaluate its difficulty level, helping you to choose materials that align with your current understanding and learning goals.
+    """)
 
-# Streamlit interface
-import streamlit as st
-# ... [Other imports remain the same]
+    # Text input for prediction
+    user_input = st.text_area("Enter the French text here:", height=80)
+    if st.button('Predict Difficulty of Text'):
+        if user_input:
+            difficulty = predict_difficulty(user_input)
+            st.success(f"The predicted difficulty level of the text is: {difficulty}")
+        else:
+            st.error("Please enter a French text for analysis.")
 
-# ... [Your existing function definitions]
-
-# Streamlit interface
-def main():
-    set_global_background_color("white")
-
-    left_column, right_column = st.columns([1, 2])
-
-    with left_column:
-        st.image("https://upload.wikimedia.org/wikipedia/commons/2/2b/Logo_Universit%C3%A9_de_Lausanne.svg", 
-                 caption="University of Lausanne")
-        st.image("https://1000logos.net/wp-content/uploads/2021/06/Zoom-Logo.png", 
-                 caption="Zoom logo")
-
-    with right_column:
-        st.title('Welcome to LingoRank!')
-
-        st.write("""
-        ### Overview
-        LingoRank is a revolutionary startup aimed at enhancing foreign language learning. Our tool helps English speakers to improve their French by reading texts that match their language proficiency level. 
-
-        Finding texts with the appropriate difficulty level (A1 to C2) can be challenging. LingoRank solves this by predicting the difficulty of French texts, aiding learners in choosing materials that are neither too easy nor too hard. 
-
-        Simply enter a French text below, or a YouTube URL for captions, and LingoRank will evaluate its difficulty level, helping you to choose materials that align with your current understanding and learning goals.
-        """)
-
-        # Text input for prediction
-        user_input = st.text_area("Enter the French text here:", height=200)
-        if st.button('Predict Difficulty of Text'):
-            if user_input:
-                difficulty = predict_difficulty(user_input)
-                st.success(f"The predicted difficulty level of the text is: {difficulty}")
-            else:
-                st.error("Please enter a French text for analysis.")
-
-        # YouTube URL input for caption analysis
-        youtube_url = st.text_input("Or enter a YouTube video URL:")
-        if st.button('Analyze YouTube Captions'):
-            if youtube_url:
-                video_id = get_video_id(youtube_url)
-                captions = get_captions_from_url(video_id)
-                if captions:
-                    if detect(captions) == 'fr':
-                        difficulty = predict_difficulty(captions)
-                        st.success(f"The predicted difficulty level of the captions is: {difficulty}")
-                    else:
-                        st.error("Captions are not in French.")
-                else:
-                    st.error("French captions not available for this video or unable to fetch captions.")
-            else:
-                st.error("Please enter a YouTube URL for analysis.")
-
-if __name__ == "__main__":
-    main()
+    # New feature: YouTube subtitle extraction and analysis
+    st.write("## Analyze YouTube Video Subtitles")
+    youtube_url = st.text_input("Enter the YouTube video URL:")
+    if st.button('Analyze YouTube Subtitles'):
+        if youtube_url:
+            result = fetch_and_analyze_subtitles(youtube_url)
+            st.success(f"The subtitles were predicted as having a french level: {result}")
+        else:
+            st.error("Please enter a YouTube video URL.")
